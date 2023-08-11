f00ccb3b39e733dc8200804faa831357ed6f41ae
Jo Matsumaru <hatunehimeka@outlook.jp>
2023-08-11 18:31:12 +0900

    Tomlファイルを作成するメソッドを追加 ビルド時にdllファイルとxmlファイルをそれぞれの名前のフォルダーに移動するように

--- RiliaBOT/Program.cs
+++ RiliaBOT/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace RiliaBOT
+﻿using System;
+using System.IO;
+
+namespace RiliaBOT
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+          AskExists_Toml();
+        }
+
+        private static void BinFile()
+        {
+            switch (Directory.Exists("./bin"))
+            {
+                case true :
+                    Console.WriteLine("Log : Dir True");
+                    break;
+                case false:
+                    Directory.CreateDirectory("./bin");
+                    Console.WriteLine("Log : Dir False");
+                    break;
+            }
+        }
+        
+        private static void AskExists_Toml()
+        {
+            switch (File.Exists("./Config.toml"))
+            {
+                case false:
+                    using (FileStream fs = File.Create("./Config.toml"))
+                    {
+                    }
+                    Console.WriteLine("Log : ConfigToml not Exists");
+                    break;
+                case true:
+                    Console.WriteLine("Log : ConfigToml Exists");
+                    break;
+            }
         }
     }
 }
